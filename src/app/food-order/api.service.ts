1f63b2a683eeab0f1e999e821a28aa3a0da73d11
Nazrul Hanif <nazrul.workspace@gmail.com>
2021-03-15 12:57:49 +0800

    hotfix payment api

--- src/app/food-order/api.service.ts
+++ src/app/food-order/api.service.ts
@@ -69,46 +69,7 @@ export class ApiService {
         return this.http.get(this.productServiceURL + url, header);
     }
 
-    // postPaymentLink(data){
-    //     const header = {
-    //         // sample 
-    //         headers: new HttpHeaders().set("Authorization", `Bearer 73e7cf38-f390-4d4b-b2e8-7f24cbcf2f46`),
-    //     };
-
-    //     console.log('Masok la weh: ', data);
-    //     // console.log('endpoint: ' + this.payServiceURL + "payments/makePayment")
-    //     return this.http.post(this.payServiceURL + "payments/makePayment", data, header);
-        
-    //     // ask taufik for ssl validation. require cert to access the API 
-    // }
-
-    // postPaymentLink(data): Observable<any> {
-    //     // const header = new HttpHeaders({
-    //     //   'Content-Type': 'application/json',
-    //     //   'Authorization': `Bearer 73e7cf38-f390-4d4b-b2e8-7f24cbcf2f46`
-    //     // });
-
-    //     const header = {
-    //         // sample 
-    //         headers: new HttpHeaders().set(
-    //             "Authorization", `Bearer 73e7cf38-f390-4d4b-b2e8-7f24cbcf2f46`),
-    //     };
-
-    //     return this.http.post(this.payServiceURL + "payments/makePayment", data, { headers: this.header });
-    //     // return this.http.post(apiUrl, data { headers: headers })
-    // }
-
-    // getCcDetailApplicationBydetail(token, app_id) {
-    //     const header = {
-    //       headers: new HttpHeaders().set("Authorization", `Bearer ${token}`),
-    //     };
-    //     const url = "cc/application/detail?id=" + app_id;
-    //     return this.http.get(this.v2APIUrl + url, header);
-    // }
-
     postPaymentLink(data):Observable<any> {
-
-        console.log(data);
 
         const httpOptions = {
             headers: new HttpHeaders(
@@ -118,7 +79,6 @@ export class ApiService {
             })
         }
 
-        console.log(httpOptions)
         const url = this.payServiceURL + "payments/makePayment";
 
         console.log('send: ', url, data, httpOptions)
@@ -126,20 +86,4 @@ export class ApiService {
         // return this.http.get(this.payServiceURL + "payments/makePayment", httpOptions);
     }
     
-
-    // getcategories():Observable<any> {
-
-    //     const httpOptions = {
-    //          headers: new HttpHeaders(
-    //          { 
-    //             'Authorization': `Bearer ${this.tokenPay}`,
-    //             'Content-Type': 'application/json'
-    //          })
-    //      }
-
-    //    return this.httpclient.get("http://localhost:8000/api/v1/categories/", httpOptions);
-
-    //  }
-    
-
 }
